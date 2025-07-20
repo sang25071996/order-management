b01d3fff1cc5c8415b15a22784ac10649a97d53d
Sang Nguyen <nguyentansang96xuanba@gmail.com>
2025-07-20 21:03:45 +0700

    fix trigger build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg'],
   build: {
     outDir: 'dist', // Output directory matches amplify.yml
     rollupOptions: {
       input: './index.html', // Explicitly set index.html as the entry point
     },
   },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
 })
